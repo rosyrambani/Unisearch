4716c8a1614e6be71177751225e58e108bb9cdd5
Sahil <sahilg14@gmail.com>
2017-05-21 16:48:06 -0400

    fixed fading

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@ Echo "<table>
 <tr style='color: indianred;'><td>Id</td><td>First Name</td><td>Last Name</td><td>Email</td><td>Experience</td><td>Country</td><td>Action</td></tr>";
 if (mysqli_num_rows($result) > 0) {
 while($row = mysqli_fetch_assoc($result)){
-Echo "<tr><td>".$row['id']."</td><td>".$row['firstname']."</td><td>".$row['lastname']."</td><td>".$row['email']."</td><td>".$row['experience']."</td><td>".$row['country']."</td><td><button name='delete' id='delete' value='".$row['id']."' onclick='deleteit(this.value)'>Delete</button></td></tr>";
+Echo "<tr><td>".$row['id']."</td><td>".$row['firstname']."</td><td>".$row['lastname']."</td><td>".$row['email']."</td><td>".$row['experience']."</td><td>".$row['country']."</td><td><button name='delete' class='delete' value='".$row['id']."'>Delete</button></td></tr>";
 }
 }
 else {
@@ -17,20 +17,22 @@ Echo "</table>";
 ?>
 
 <script>
-function deleteit(idofbutton){
+$(document).ready(function(){
+$('.delete').click(function(){
 	console.log("invoker");
-var invoker= idofbutton ;
-
+var invoker= $(this).attr('value') ;
+var line = $(this).parent().parent();
  $.ajax({
         url: 'delete.php',
         type: 'POST',
         data:'post_id=' + invoker,
     	success: function() {
-          location.reload();
+    		line.fadeOut('slow');
         },
         error: function() {
           alert("There was an error. Try again please!");
         }
     });
-}
+});
+});
 </script>
